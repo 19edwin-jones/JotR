327f70298eac349a8b5179b2ebcae12488f82af6
Edwin Jones <19edwin.jones@gmail.com>
2026-08-29 18:28:49 -0400

    Implement basic CRUD functionality

--- jotr-core/src/lib.rs
+++ jotr-core/src/lib.rs
@@ -13,6 +13,12 @@ impl NoteStore {
     pub fn new() -> Self {
         Self::default()
     }
+
+    // read all notes
+    pub fn notes(&self) -> &[Note] {
+        &self.notes
+    }
+
     pub fn add_note(&mut self, content: String) {
         let note = Note {
             id: self.notes.len() as u64,
@@ -21,14 +27,26 @@ impl NoteStore {
         self.notes.push(note);
     }
 
-    // read all notes
-    pub fn notes(&self) -> &[Note] {
-        &self.notes
-    }
-
     // read a specifc note
     pub fn get_note(&self, id: u64) -> Option<&Note> {
         self.notes.iter().find(|note| note.id == id)
+    }
+
+    pub fn update_note(&mut self, id: u64, new_content: String) -> Option<&Note> {
+        if let Some(note) = self.notes.iter_mut().find(|note| note.id == id) {
+            note.content = new_content;
+            Some(note)
+        } else {
+            None
+        }
+    }
+
+    pub fn delete_note(&mut self, id: u64) -> Option<Note> {
+        if let Some(pos) = self.notes.iter().position(|note| note.id == id) {
+            Some(self.notes.remove(pos))
+        } else {
+            None
+        }
     }
 }
 
@@ -48,8 +66,40 @@ mod tests {
 
     #[test]
     fn can_get_note_by_id() {
-        let  store = NoteStore::new();
+        let store = NoteStore::new();
 
         assert_eq!(store.get_note(999), None);
+    }
+
+    #[test]
+    fn can_update_note() {
+        let mut store = NoteStore::new();
+
+        store.add_note("Hello JotR".to_string());
+
+        assert_eq!(
+            store.update_note(0, "Updated content".to_string()),
+            Some(&Note {
+                id: 0,
+                content: "Updated content".to_string()
+            })
+        );
+        assert_eq!(store.notes()[0].content, "Updated content");
+    }
+
+    #[test]
+    fn can_delete_note() {
+        let mut store = NoteStore::new();
+
+        store.add_note("Hello JotR".to_string());
+
+        assert_eq!(
+            store.delete_note(0),
+            Some(Note {
+                id: 0,
+                content: "Hello JotR".to_string()
+            })
+        );
+        assert_eq!(store.notes().len(), 0);
     }
 }
